a5751054f7e29b14381c969dc08273f96aaa4431
wknudson <wknudson@byu.edu>
2026-01-28 10:08:19 -0700

    names and section top comment

--- Program.cs
+++ Program.cs
@@ -1,25 +1,5 @@
-﻿// 1) The “Driver” class (the Program.cs class with the main method where the program begins) will:
-//• Welcome the user to the game
-//• Create a game board array to store the players’ choices
-//• Ask each player in turn for their choice and update the game board array
-//• Print the board by calling the method in the supporting class
-//• Check for a winner by calling the method in the supporting class, and notify the players
-//when a win has occurred and which player won the game
-
-
-//using System.ComponentModel;
-
-//Contain a method that prints the board based on the information passed to it 
-//public void printCurrentBoard(char[] board)
-
-//Contain a method that receives the game board array as input and returns if there is a winner and who it was
-//public char checkWin(char[] board)
-//Return ‘x’ ‘o’ or ‘d’ (else = no win)
-
-
-//As always, write good, clean code with descriptive variable names, proper indentation, and clear
-//commenting as appropriate to describe what the code is doing.
-
+﻿//Section 1 group 10
+//James, Josh, Brady, & Will
 using Mission4Assignment;
 
 GameTools gt = new GameTools();
