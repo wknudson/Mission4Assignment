dc6f6d0850e28244e20063324f0cc9619473fb81
Joshesol <joshuaericsolano@gmail.com>
2026-01-28 09:21:04 -0700

    created first itteration of printCurrentBoard

--- GameTools.cs
+++ GameTools.cs
@@ -12,5 +12,20 @@ namespace Mission4Assignment
 {
     internal class GameTools
     {
+        public void printCurrentBoard(char[] board)
+        {
+            Console.WriteLine(board[0] + "|" + board[1] + "|" + board[2]);
+            Console.WriteLine("------");
+            Console.WriteLine(board[3] + "|" + board[4] + "|" + board[5]);
+            Console.WriteLine("------");
+            Console.WriteLine(board[6] + "|" + board[7] + "|" + board[8]);
+            
+        }
+
+        public char checkWin(char[] board)
+        {
+            return result;
+        }
+
     }
 }
