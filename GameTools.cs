0e97791ebde98ddb2e90d09b02ec294b44af3713
wknudson <wknudson@byu.edu>
2026-01-28 08:53:30 -0700

    Add project files.

---
+++ GameTools.cs
@@ -0,0 +1,16 @@
+﻿//2) The supporting class (with whichever name you choose) will:
+//• Receive the “board” array from the driver class
+//• Contain a method that prints the board based on the information passed to it
+//• Contain a method that receives the game board array as input and returns if there is a
+//winner and who it was
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission4Assignment
+{
+    internal class GameTools
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// 1) The “Driver” class (the Program.cs class with the main method where the program begins) will:
+//• Welcome the user to the game
+//• Create a game board array to store the players’ choices
+//• Ask each player in turn for their choice and update the game board array
+//• Print the board by calling the method in the supporting class
+//• Check for a winner by calling the method in the supporting class, and notify the players
+//when a win has occurred and which player won the game
+
+//As always, write good, clean code with descriptive variable names, proper indentation, and clear
+//commenting as appropriate to describe what the code is doing.
+
+
